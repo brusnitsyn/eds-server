e7171ea88bcfbab9b16780d9ebd0d1010a0c1ff7
brusnitsyn <brusnitsyn@github.com>
2024-09-26 14:20:15 +0900

    [н]: входящие параметры запросов, для поиска и фильтрации

--- app/Http/Controllers/Api/v1/StaffController.php
+++ app/Http/Controllers/Api/v1/StaffController.php
@@ -10,12 +10,38 @@ use App\Http\Resources\Staff\ShortStaffCollection;
 use App\Http\Resources\Staff\ShortStaffResource;
 use App\Models\Staff;
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 
 class StaffController extends Controller
 {
-    public function all()
+    public function all(Request $request)
     {
-        $staff = Staff::paginate(30);
+        $query = Staff::query();
+        $searchColumn = $request->query('search_from');
+        $searchValue = $request->query('search_value');
+        $validType = $request->query('valid_type');
+
+        if (!is_null($validType)) {
+            switch ($validType) {
+                case 'no-valid':
+                    $query->whereHas('certification', function ($query) {
+                        $query->where('is_valid', false);
+                    });
+                    break;
+                case 'new-request':
+                    $query->whereHas('certification', function ($query) {
+                        $query->where('is_request_new', true);
+                    });
+                    break;
+            }
+        }
+
+        if (!is_null($searchColumn) && !is_null($searchValue)) {
+            $query->where($searchColumn, 'ilike', $searchValue . '%');
+        }
+
+        $staff = $query->paginate(30);
+
         return new ShortStaffCollection($staff);
     }
 
