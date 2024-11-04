a0715f6a5f293285b58b793aeb6e0f9c6fcc2fe5
brusnitsyn <brusnitsyn@github.com>
2024-11-04 22:20:11 +0900

    Учетки персонала

---
+++ app/Models/StaffIntegrate.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class StaffIntegrate extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'login',
+        'password',
+        'link',
+        'staff_id'
+    ];
+
+    public function staff(): \Illuminate\Database\Eloquent\Relations\HasOne
+    {
+        return $this->hasOne(Staff::class);
+    }
+}
